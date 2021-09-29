6d0a7e57798c22e25dcf681d9922196398b27a6d
Matt Lane <lane.matthew@gmail.com>
2021-09-29 15:02:48 -0700

    add random fact on page load

--- src/Number.js
+++ src/Number.js
@@ -1,9 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import NumberForm from "./NumberForm";
 import NumberFactList from "./NumberFactList";
 
 function Number() {
   const [facts, setFacts] = useState([]);
+  const [randomFact, setRandomFact] = useState(null);
+  // console.log("i'm rendering number");
+
+  // useEffect(() => {
+  //   console.log("i'm an effect!!!");
+  // });
+
+  useEffect(() => {
+    // console.log("i'm number's useEffect");
+    async function getRandomFact() {
+      const response = await fetch(`http://numbersapi.com/random/trivia?json`);
+      const body = await response.json();
+      setRandomFact(body);
+    }
+    getRandomFact();
+  }, []);
 
   const addFact = fact => {
     setFacts(oldFacts => [fact, ...oldFacts]);
@@ -11,6 +27,9 @@ function Number() {
 
   return (
     <div>
+      <div>
+        Here's a random fact: <p>{randomFact?.text}</p>
+      </div>
       <NumberForm addFact={addFact} />
       <NumberFactList facts={facts} />
     </div>
